67b4771960cf8cd2f3daece9192e63fd989bf030
Eric Liu <eliu@yelp.com>
2014-06-19 15:43:22 -0700

    cleaning up js error print

--- moe/static/js/exception.js
+++ moe/static/js/exception.js
@@ -2,8 +2,8 @@ var optimalLearning = optimalLearning || {};
 
 optimalLearning.errorAlert = function(jqXHR, textStatus, errorThrown) {
   if (jqXHR.responseText.indexOf('DOCTYPE') !== -1){
-    alert("INTERNAL 500 ERROR\nCheck console.");
+    alert("INTERNAL ERROR\nCheck console.");
   } else {
-    alert("500 ERROR\n" + jqXHR.responseText);
+    alert(jqXHR.responseText);
   }
 };
