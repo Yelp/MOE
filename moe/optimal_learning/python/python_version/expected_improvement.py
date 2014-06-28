fixing 0/0 1d-analytic error in Python by adding minmum values for variance/deviations (see prev commit for C++ equiv)

moe/optimal_learning/python/python_version/expected_improvement.py
@@ -12,6 +12,7 @@ import scipy.stats
 from moe.optimal_learning.python.constant import DEFAULT_EXPECTED_IMPROVEMENT_MC_ITERATIONS, DEFAULT_MAX_NUM_THREADS
 from moe.optimal_learning.python.interfaces.expected_improvement_interface import ExpectedImprovementInterface
 from moe.optimal_learning.python.interfaces.optimization_interface import OptimizableInterface
+from moe.optimal_learning.python.python_version.gaussian_process import GaussianProcess
 from moe.optimal_learning.python.python_version.optimization import multistart_optimize, NullOptimizer
 
 
@@ -85,6 +86,14 @@ class ExpectedImprovement(ExpectedImprovementInterface, OptimizableInterface):
     See interfaces/expected_improvement_interface.py docs for further details.
 
     """
+
+    # Minimum allowed variance value in the "1D" analytic EI and grad EI computations.
+    # Values that are too small result in problems b/c we may compute ``std_dev/var`` (which is enormous
+    # if ``std_dev = 1.0e-150`` and ``var = 1.0e-300``) since this only arises when we fail to compute ``std_dev = var = 0.0``.
+    # Note: this is only relevant if noise = 0.0; this minimum will not affect EI computation with noise since this value
+    # is below the smallest amount of noise users can meaningfully add.
+    MINIMUM_VARIANCE_EI = numpy.finfo(numpy.float64).tiny
+    MINIMUM_VARIANCE_GRAD_EI = 150 * GaussianProcess.MINIMUM_STD_DEV ** 2
 
     def __init__(
             self,
@@ -586,6 +595,7 @@ class ExpectedImprovement(ExpectedImprovementInterface, OptimizableInterface):
         var_star = self._gaussian_process.compute_variance_of_points(union_of_points)
 
         if num_points == 1 and force_monte_carlo is False:
+            var_star = numpy.fmax(self.MINIMUM_VARIANCE_EI, var_star)
             return self._compute_expected_improvement_1d_analytic(mu_star[0], var_star[0, 0])
         else:
             return self._compute_expected_improvement_monte_carlo(mu_star, var_star)
@@ -626,9 +636,16 @@ class ExpectedImprovement(ExpectedImprovementInterface, OptimizableInterface):
         mu_star = self._gaussian_process.compute_mean_of_points(union_of_points)
         var_star = self._gaussian_process.compute_variance_of_points(union_of_points)
         grad_mu = self._gaussian_process.compute_grad_mean_of_points(union_of_points, self.num_to_sample)
-        grad_chol_decomp = self._gaussian_process.compute_grad_cholesky_variance_of_points(union_of_points, self.num_to_sample)
 
         if num_points == 1 and force_monte_carlo is False:
+            var_star = numpy.fmax(self.MINIMUM_VARIANCE_GRAD_EI, var_star)
+            sigma = numpy.sqrt(var_star)
+            grad_chol_decomp = self._gaussian_process.compute_grad_cholesky_variance_of_points(
+                union_of_points,
+                chol_var=sigma,
+                num_derivatives=self.num_to_sample,
+            )
+
             return self._compute_grad_expected_improvement_1d_analytic(
                 mu_star[0],
                 var_star[0, 0],
@@ -636,6 +653,12 @@ class ExpectedImprovement(ExpectedImprovementInterface, OptimizableInterface):
                 grad_chol_decomp[0, 0, 0, ...],
             )
         else:
+            chol_var = scipy.linalg.cho_factor(var_star, lower=True, overwrite_a=True)[0]
+            grad_chol_decomp = self._gaussian_process.compute_grad_cholesky_variance_of_points(
+                union_of_points,
+                chol_var=chol_var,
+                num_derivatives=self.num_to_sample,
+            )
             return self._compute_grad_expected_improvement_monte_carlo(
                 mu_star,
                 var_star,

moe/optimal_learning/python/python_version/gaussian_process.py
@@ -53,6 +53,13 @@ class GaussianProcess(GaussianProcessInterface):
 
     """
 
+    # Minimum allowed standard deviation value in ComputeGradCholeskyVarianceOfPointsPerPoint (= machine precision).
+    # Values that are too small result in problems b/c we may compute ``std_dev/var`` (which is enormous
+    # if ``std_dev = 1.0e-150`` and ``var = 1.0e-300``) since this only arises when we fail to compute ``std_dev = var = 0.0``.
+    # Note: this is only relevant if noise = 0.0; this minimum will not affect GPs with noise since this value
+    # is below the smallest amount of noise users can meaningfully add.
+    MINIMUM_STD_DEV = numpy.finfo(numpy.float64).eps
+
     def __init__(self, covariance_function, historical_data):
         """Construct a GaussianProcess object that knows how to call C++ for evaluation of member functions.
 
@@ -297,7 +304,7 @@ class GaussianProcess(GaussianProcessInterface):
         # Step 2 of Appendix 2
         for k in xrange(num_to_sample):
             L_kk = chol_var[k, k]
-            if L_kk > 1.0e-16:
+            if L_kk > self.MINIMUM_STD_DEV:
                 grad_chol[k, k, ...] *= 0.5 / L_kk
                 for j in xrange(k + 1, num_to_sample):
                     grad_chol[j, k, ...] = (grad_chol[j, k, ...] - chol_var[j, k] * grad_chol[k, k, ...]) / L_kk
@@ -307,7 +314,7 @@ class GaussianProcess(GaussianProcessInterface):
 
         return grad_chol
 
-    def compute_grad_cholesky_variance_of_points(self, points_to_sample, num_derivatives=-1):
+    def compute_grad_cholesky_variance_of_points(self, points_to_sample, chol_var=None, num_derivatives=-1):
         r"""Compute the gradient of the cholesky factorization of the variance (matrix) of this GP at each point of ``Xs`` (``points_to_sample``) wrt ``Xs``.
 
         .. Warning:: ``points_to_sample`` should not contain duplicate points.
@@ -324,6 +331,8 @@ class GaussianProcess(GaussianProcessInterface):
 
         :param points_to_sample: num_to_sample points (in dim dimensions) being sampled from the GP
         :type points_to_sample: array of float64 with shape (num_to_sample, dim)
+        :param chol_var: the cholesky factorization (L) of the variance matrix; only the lower triangle is accessed
+        :type chol_var: array of float64 with shape (num_to_sample, num_to_sample)
         :param num_derivatives: return derivatives wrt points_to_sample[0:num_derivatives]; large or negative values are clamped
         :type num_derivatives: int
         :return: grad_chol: gradient of the cholesky factorization of the variance matrix of this GP.
@@ -334,9 +343,12 @@ class GaussianProcess(GaussianProcessInterface):
 
         """
         num_derivatives = self._clamp_num_derivatives(points_to_sample.shape[0], num_derivatives)
-        var_star = self.compute_variance_of_points(points_to_sample)
-        # Note: only access the lower triangle of chol_var; upper triangle is garbage
-        chol_var = scipy.linalg.cho_factor(var_star, lower=True, overwrite_a=True)[0]
+
+        # Users can pass this in directly b/c it has often been computed already.
+        if chol_var is None:
+            var_star = self.compute_variance_of_points(points_to_sample)
+            # Note: only access the lower triangle of chol_var; upper triangle is garbage
+            chol_var = scipy.linalg.cho_factor(var_star, lower=True, overwrite_a=True)[0]
 
         grad_chol_decomp = numpy.empty((num_derivatives, points_to_sample.shape[0], points_to_sample.shape[0], self.dim))
         for i in xrange(num_derivatives):