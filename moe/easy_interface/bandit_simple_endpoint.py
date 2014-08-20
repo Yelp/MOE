0ad80f9592778bc41a36d30099f6faf8c7094857
norases <norases@gmail.com>
2014-08-20 16:19:19 -0700

    Started bandit easy_interface

---
+++ moe/easy_interface/bandit_simple_endpoint.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""Simple functions for hitting the REST endpoints of a MOE bandit service."""
+import simplejson as json
+
+from moe.easy_interface import call_endpoint_with_payload, DEFAULT_HOST, DEFAULT_PORT
+from moe.bandit.data_containers import HistoricalData
+from moe.views.constant import ALL_REST_ROUTES_ROUTE_NAME_TO_ENDPOINT, BANDIT_BLA_ROUTE_NAME, BANDIT_EPSILON_ROUTE_NAME, BANDIT_UCB_ROUTE_NAME
+from moe.views.schemas.bandit_pretty_view import BanditResponse
+
+
+def bandit(
+        historical_info,
+        bandit_route_name=BANDIT_BLA_ROUTE_NAME,
+        subtype=DEFAULT_BLA_SUBTYPE,
+        rest_host=DEFAULT_HOST,
+        rest_port=DEFAULT_PORT,
+        testapp=None,
+        **kwargs
+        ):
+    """Hit the rest endpoint for allocating arms of a bandit given arms already sampled (historical info)."""
+    endpoint = ALL_REST_ROUTES_ROUTE_NAME_TO_ENDPOINT[bandit_route_name]
+    raw_payload = kwargs.copy()
+
+    json_payload = json.dumps(raw_payload)
+
+    json_response = call_endpoint_with_payload(rest_host, rest_port, endpoint, json_payload, testapp)
+
+    output = BanditResponse().deserialize(json_response)
+
+    return output['arm_allocations']
